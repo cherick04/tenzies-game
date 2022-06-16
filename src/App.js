0ab93815d8525d02620a75c858f6357e26be3058
Erick C <cherick04@hotmail.com>
2022-06-15 20:19:03 -0500

    Added time tracking

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import Confetti from 'react-confetti'
 export default function App() {
   const [dice, setDice] = useState(allNewDice())
   const [tenzies, setTenzies] = useState(false)
+  const [rolls, setRolls] = useState(0)
+  const [time, setTime] = useState(() => new Date())
+  const [bestTime, setBestTime] = useState(
+    () => JSON.parse(localStorage.getItem('bestTime')) || []
+  )
 
   useEffect(() => {
     const firstValue = dice[0].value
     const hasWon = dice.every((die) => die.isHeld && die.value === firstValue)
     if (hasWon) {
       setTenzies(true)
-      console.log('You won!')
+      setTime((oldTime) => new Date() - oldTime)
     }
   }, [dice])
+
+  // useEffect(() => {
+  //   if (tenzies) {
+  //     localStorage.setItem('bestTime', JSON.stringify(bestTime))
+  //   }
+  // }, [tenzies])
 
   function generateNewDie() {
     return {
@@ -36,12 +47,18 @@ export default function App() {
     if (tenzies) {
       setDice(allNewDice())
       setTenzies(false)
+      setRolls(0)
+      setTime(new Date())
     } else {
       setDice((oldDice) =>
         oldDice.map((die) => {
           return die.isHeld ? die : generateNewDie()
         })
       )
+      setRolls((prevRolls) => prevRolls + 1)
+    }
+    if (dice.every((die) => !die.isHeld)) {
+      setTime(new Date())
     }
   }
 
@@ -51,6 +68,19 @@ export default function App() {
         return die.id === id ? { ...die, isHeld: !die.isHeld } : die
       })
     )
+  }
+
+  function formatTime(elapsed) {
+    var milliseconds = Math.floor((elapsed % 1000) / 100),
+      seconds = Math.floor((elapsed / 1000) % 60),
+      minutes = Math.floor((elapsed / (1000 * 60)) % 60),
+      hours = Math.floor((elapsed / (1000 * 60 * 60)) % 24)
+
+    hours = hours < 10 ? '0' + hours : hours
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    return hours + ':' + minutes + ':' + seconds + '.' + milliseconds
   }
 
   const diceElements = dice.map((die) => (
@@ -63,19 +93,29 @@ export default function App() {
   ))
 
   return (
-    <main>
+    <>
       {tenzies && (
-        <Confetti width={window.innerWidth} height={window.innerHeight} />
+        <h2 className="winning--lbl">
+          You won with only {rolls} rolls!
+          <br />
+          Your time: {formatTime(time)}
+        </h2>
       )}
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">
-        Roll until all dice are the same. Click each die to freeze it at its
-        current value between rolls.
-      </p>
-      <div className="dice-grid">{diceElements}</div>
-      <button className="roll--button" onClick={rollDice}>
-        {tenzies ? 'New Game' : 'Roll'}
-      </button>
-    </main>
+      <main>
+        {tenzies && (
+          <Confetti width={window.innerWidth} height={window.innerHeight} />
+        )}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls. To start timer unselect all dice and
+          click on 'Roll'
+        </p>
+        <div className="dice-grid">{diceElements}</div>
+        <button className="roll--button" onClick={rollDice}>
+          {tenzies ? 'New Game' : 'Roll'}
+        </button>
+      </main>
+    </>
   )
 }
